1ecac3fab9bc135b5e7dfb357eabb049912892d8
ChangwenLuo <changwen.luo@shopee.com>
2020-10-26 20:56:01 +0800

    update: 更新release sh

--- deploy/bash.sh
+++ deploy/bash.sh
@@ -3,6 +3,6 @@
 echo "Start to deploy mac-music"
 
 yarn build
-scp -r ./dist/** root@47.100.138.80:/usr/share/nginx/html
+scp -r ./dist/** root@47.100.138.80:/luo/mac-music
 
 echo "Successfully deploy mac-music"
